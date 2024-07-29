fd50a56d2feb670d34970db8c6c16a71dd898b82
Jonathan Perkins <jperkin4@uab.edu>
2024-07-28 23:46:42 -0500

    Created MST with Prim's algorithm

--- minimum-spanning-tree/src/Main.java
+++ minimum-spanning-tree/src/Main.java
@@ -1,6 +1,5 @@
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
@@ -11,8 +10,44 @@ public class Main {
         // read tinyDG.txt to a weighted graph
 
         WeightedGraph.Graph tinyDG = fileToWeightedGraph("tinyDG.txt");
+        System.out.println("tinyDG has " + tinyDG.getNodes() + " nodes and " + tinyDG.getEdges() + " edges.");
         tinyDG.printGraph();
-        System.out.println(tinyDG.nodes);
+        tinyDG.primMST();
+
+        // PRIM'S ALGORITHM
+
+        WeightedGraph.Graph mediumDG = fileToWeightedGraph("mediumDG.txt");
+        WeightedGraph.Graph largeDG = fileToWeightedGraph("largeDG.txt");
+        WeightedGraph.Graph XtraLargeDG = fileToWeightedGraph("XtraLargeDG.txt");
+
+        // RECORDING MST TIME
+
+        double[] times = new double[4];
+
+        double tick = System.nanoTime();
+        tinyDG.primMST();
+        double tock = System.nanoTime();
+        times[0] = ((tock - tick) / 1_000_000_000);
+
+        tick = System.nanoTime();
+        mediumDG.primMST();
+        tock = System.nanoTime();
+        times[1] = ((tock - tick) / 1_000_000_000);
+
+        tick = System.nanoTime();
+        largeDG.primMST();
+        tock = System.nanoTime();
+        times[2] = ((tock - tick) / 1_000_000_000);
+
+        tick = System.nanoTime();
+        XtraLargeDG.primMST();
+        tock = System.nanoTime();
+        times[3] = ((tock - tick) / 1_000_000_000);
+
+        System.out.println("Times (in seconds): ");
+        for (double time : times) {
+            System.out.println(time + ", ");
+        }
 
     }
 
@@ -20,19 +55,22 @@ public class Main {
 
         Scanner sc = new Scanner(new File(inputFile));
 
-        int first = sc.nextInt();
+        int nodes = sc.nextInt();
+        sc.nextLine();
 
         // second integer is the number of edges
         int edges = sc.nextInt();
+        sc.nextLine();
 
-        WeightedGraph.Graph output = new WeightedGraph.Graph(edges);
+        WeightedGraph.Graph output = new WeightedGraph.Graph(nodes, edges);
 
         while (sc.hasNextLine()) { // add all the edges
             String line = sc.nextLine();
             if (line.isEmpty()) break;
-            String[] splitLine = line.split(" ");
+            String[] splitLine = line.trim().split(" +");
             output.addEgde(Integer.parseInt(splitLine[0]), Integer.parseInt(splitLine[1]),
                     Double.parseDouble(splitLine[2]));
+
         }
 
         return output;
--- minimum-spanning-tree/src/WeightedGraph.java
+++ minimum-spanning-tree/src/WeightedGraph.java
@@ -1,4 +1,5 @@
 import java.util.LinkedList;
+import java.util.Arrays;
 
 public class WeightedGraph {
 
@@ -15,11 +16,12 @@ public class WeightedGraph {
     }
 
     public static class Graph {
-        int nodes;
+        int nodes, edges;
         LinkedList<Edge> [] adjacencyList;
 
-        Graph(int nodes) {
+        Graph(int nodes, int edges) {
             this.nodes = nodes;
+            this.edges = edges;
             adjacencyList = new LinkedList[nodes];
 
             for (int i = 0; i < nodes ; i++) { // initialize an adjacency lists for every node
@@ -28,17 +30,80 @@ public class WeightedGraph {
         }
 
         public void addEgde(int source, int destination, double weight) {
-            Edge edge = new Edge(source, destination, weight);
-            adjacencyList[source].addFirst(edge); // for directed graph
+            adjacencyList[source].add(new Edge(source, destination, weight)); // add edge to node on adjacency list
         }
 
         public void printGraph(){
+
             for (int i = 0; i < nodes; i++) {
+
                 LinkedList<Edge> list = adjacencyList[i];
+
                 for (Edge edge : list) {
-                    System.out.println("Vertex " + i + " is connected to " +
+                    System.out.println("Node " + edge.source + " is connected to node " +
                             edge.destination + " with weight " + edge.weight);
                 }
+            }
+        }
+
+        public int getNodes() {
+            return this.nodes;
+        }
+
+        public int getEdges() {
+            return this.edges;
+        }
+
+        public void primMST() { // Prim's algorithm for a Minimum Spanning Tree
+
+            boolean[] selected = new boolean[nodes]; // array to track selected node
+            Arrays.fill(selected, false); // selected node becomes true, all else are false
+            selected[0] = true; // start with the first node
+
+            int numberOfEdges = 0; // number of edges will always be < (nodes - 1)
+            System.out.println("Edge\t\tWeight");
+
+            while (numberOfEdges < nodes - 1) {
+
+                // find all adjacent nodes, calculate distance from initial node (selected[0]),
+                // discard if already in the set, otherwise choose another node closest to initial node
+
+                double min = Double.MAX_VALUE; // to find minimum weight
+                int x = 0; // row number
+                int y = 0; // column number
+
+                for (int i = 0; i < nodes; i++) {
+
+                    if (selected[i]) {
+
+                        for (int j = 0; j < adjacencyList[i].size(); j++) {
+
+                            //if (!selected[j]) { // node is not selected and has an edge
+                            for (Edge edge : adjacencyList[i]) {
+
+                                if (edge.destination == adjacencyList[i].get(j).source) {
+
+                                    if (!selected[j]) { // node is not in selected set and has an edge
+
+                                        if (min > adjacencyList[i].get(j).weight) {
+
+                                            min = adjacencyList[i].get(j).weight;
+                                            x = i;
+                                            y = j;
+                                        }
+                                    }
+
+                                }
+                            }
+
+
+                        }
+                    }
+                }
+                System.out.println(adjacencyList[x].get(y).source + " - " + adjacencyList[x].get(y).destination
+                        + "\t\t" + adjacencyList[x].get(y).weight);
+                selected[y] = true;
+                numberOfEdges++;
             }
         }
     }
